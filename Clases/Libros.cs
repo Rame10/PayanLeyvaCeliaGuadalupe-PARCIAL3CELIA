cafaabf4d958dffdeac1f0812fb85260cdde70ad
Celia Payan Leyva <celiagpe10@gmail.com>
2024-11-29 10:19:38 -0700

    Avance y correción de error en el catch

---
+++ Clases/Libros.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal_Biblioteca.Clases
+{
+    public class Libros
+    {
+        ConexionSQL x = new ConexionSQL();
+        SqlConnection con = new SqlConnection();
+
+        public int id,idAutor,idGenero,idEditorial,idCategoría;
+        public string Titulo, ISBN;
+
+        public Libros()
+        {
+            con.ConnectionString = x.Conexion;
+        }
+
+
+    }
+}
